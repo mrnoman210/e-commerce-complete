2dcd93d1adc4c2a0e144f1a0e25d91e0f182d454
MRNoman <na2458443@gmail.com>
2023-07-22 00:34:45 -0700

    after get static props adding

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,16 @@
 import Head from "next/head";
-// import Image from 'next/image'
-// import { Inter } from 'next/font/google'
 import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from "react";
 import Link from "next/link";
-// const inter = Inter({ subsets: ['latin'] })
-export default function Home() {
+export default function Home({ data }) {
   const { main_home, main_heading, price_p, loader } = styles;
-  const [data, setData] = useState()
-  useEffect(() => {
-    fetch("/api/getAllProducts", { method: "GET" })
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.log(error))
-  }, [])
+  // const [data, setData] = useState()
+  // useEffect(() => {
+  //   fetch("/api/getAllProducts", { method: "GET" })
+  //     .then(response => response.json())
+  //     .then(data => setData(data))
+  //     .catch(error => console.log(error))
+  // }, [])
   return (
     <>
       <Head>
@@ -22,7 +19,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <div className={main_home}>
 
         {!data ? <div className={loader}></div> : data.map((item, i) =>
@@ -31,9 +28,22 @@ export default function Home() {
             <p>{item.description}</p>
             <p className={price_p}> Rs.<b>{item.price}</b></p>
           </Link>
-        )} 
+        )}
 
       </div>
     </>
   );
+}
+
+export async function getStaticProps() {
+  // Fetch data from the API
+  const apiURL = "http://localhost:3000/api/getAllProducts";
+  const response = await fetch(apiURL, { method: "GET" });
+  const data = await response.json();
+  return {
+    props: {
+      data,
+    },
+    revalidate: 120,
+  };
 }
